0f1b593306c6677b3a6c38ceb1550606ea654dc7
kdong1228 <kdong1228@gmail.com>
2025-09-25 17:01:13 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from flask import Flask, render_template     # render_template 이라는 부품을 추가로 가져옵니다.
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():  # <-- 'def'로 시작하는 함수
-    return "Hello, my highway project is running!"  # <-- 함수 안에 'return' 구문
+    
+    # index.html 파일을 찾아서 사용자에게 보여줍니다.
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
